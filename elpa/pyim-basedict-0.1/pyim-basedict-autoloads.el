5fa297f0ff2dea7130823f20435c9bf4bb2732ee
LunaElf <flameyeelf@gmail.com>
2017-07-29 21:14:28 +0800

    init

---
+++ elpa/pyim-basedict-0.1/pyim-basedict-autoloads.el
@@ -0,0 +1,27 @@
+;;; pyim-basedict-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "chinese-pyim-basedict" "chinese-pyim-basedict.el"
+;;;;;;  (22908 27020 632112 870000))
+;;; Generated autoloads from chinese-pyim-basedict.el
+
+(autoload 'chinese-pyim-basedict-enable "chinese-pyim-basedict" "\
+Add basedict to chinese-pyim.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("pyim-basedict-pkg.el") (22908 27020 786726
+;;;;;;  99000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; pyim-basedict-autoloads.el ends here
---
+++ elpa/pyim-basedict-0.1/pyim-basedict-pkg.el
@@ -0,0 +1,4 @@
+(define-package "pyim-basedict" "0.1" "No description available." 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
